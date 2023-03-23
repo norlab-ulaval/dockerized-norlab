e6c505802277fe7ad3a2d29a69a9a0f03dbfe200
RedLeaderOne <luc.coupal.1@ulaval.ca>
2023-03-23 00:12:03 -0400

    NMO-161 | Implemented a post Jetpack install config script;

---
+++ jetson_xavier_install.bash
@@ -0,0 +1,98 @@
+#!/bin/bash -i
+# ////Jetson Xavier install script//////////////////////////////////////////////////////////////////////////////////
+
+# Ref:
+#  - Jetpack SDK › https://developer.nvidia.com/embedded/jetpack
+#  - Jetson wiki › https://elinux.org/Jetson
+#  - Xavier AGX wiki › https://elinux.org/Jetson_AGX_Xavier
+#  - Xavier NX wiki › https://elinux.org/Jetson_Xavier_NX
+#  - YouTrack Knowledge base › https://norlab.youtrack.cloud/articles/SAR-A-14/PRIVATE-SNOW-Jetson-Xavier-setup
+
+# ----Install instruction------------------------------------------------------------------------------------------
+# 1. Follow Jetpack install instruction at https://developer.nvidia.com/embedded/jetpack
+#    Note: Xavier NX require first updating its QSPI before using the JetPack 5.x SD Card image
+# 2. Copy the `jetson_xavier_install.bash` script into the Jetson xavier using the following command
+#       $ export JETSON_USERNAME=snow
+#       $ export JETSON_IP=10.0.0.207
+#       $ scp jetson_xavier_install.bash "$JETSON_USERNAME"@"$JETSON_IP":/home/"$JETSON_USERNAME"
+# 3. ssh in the xavier and execute the script:
+#       $ ssh "$JETSON_USERNAME"@"$JETSON_IP"
+#       $ cd /home/"$JETSON_USERNAME"/
+#       $ sudo bash jetson_xavier_install.bash
+# 4. Register your ssh credential in the Jetson
+#       $ scp ~/.ssh/id_rsa.pub "$JETSON_USERNAME"@"$JETSON_IP":./.ssh/authorized_keys
+
+JETSON_USER='snow'
+
+# ....CUDA nvcc check (for Xavier-nx via micro-sd card install)....................................................
+(
+  echo "# CUDA nvcc check (for Xavier-nx via micro-sd card install)"
+  echo "export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}"
+  echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+) >>"${JETSON_USER}"/.bashrc
+
+# Check nvcc version
+bash nvcc -V
+
+# ....Install utilities............................................................................................
+sudo apt-get update &&
+  sudo apt-get upgrade &&
+  sudo apt-get install --assume-yes \
+    tmux \
+    htop \
+    curl \
+    vim \
+    less \
+    tree \
+    apt-utils \
+    zip gzip tar unzip &&
+  sudo rm -rf /var/lib/apt/lists/*
+
+# ....Install Jetson utilities......................................................................................
+sudo pip3 install -U jetson-stats
+
+# ....Configure Docker..............................................................................................
+sudo docker version
+sudo nvidia-docker version
+
+# . . Manage Docker as a non-root user. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+sudo usermod -a -G docker "${JETSON_USER}"
+
+# . . Configure Docker to start on boot with systemd. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+sudo systemctl restart docker
+
+# ....Install Docker-compose........................................................................................
+# . . Add Docker’s official GPG key:. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# . . set up the repository:. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+# . . Finally install docker-compose. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+sudo apt-get update &&
+  sudo apt-get upgrade &&
+  sudo apt-get install --assume-yes docker-compose-plugin
+
+docker compose version
+
+# FINAL STEP
+# ....Register your ssh credential..................................................................................
+mkdir -p ~/.ssh
+
+echo "1. Dont forget to register your ssh credential in the Jetson using the following command"
+echo "   $ scp ~/.ssh/id_rsa.pub snow@10.0.0.207:./.ssh/authorized_keys"
+echo ""
+echo "2. Change the sudo timeout"
+echo "   $ sudo visudo"
+echo "   This will open a config file in vim. Change the line 'Defaults env_reset' for"
+echo "   >>> Defaults        env_reset, timestamp_timeout=300"
+echo "   Note: 300=5 hour"
+echo ""
+echo "3. When done reboot the Jetson"
+echo "$ sudo shutdown --reboot now"
