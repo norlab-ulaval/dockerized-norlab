815b800298aef8519e6cd0717a6eb9ac9ea0ecf8
RedLeaderOne <luc.coupal.1@ulaval.ca>
2023-09-25 00:19:46 -0400

    NMO-337 | Refactor /etc/profile.d/02-dn-source-ros-system-wide.sh file creation as a dockerfile step

--- dockerized-norlab-images/core-images/dn-project/project-develop/01-dn-source-ros.sh
+++
@@ -1,6 +0,0 @@
-
-# Execute ros sourcing system wide instead of in .bashrc which is not executed
-# in non-interactive shell such as in pycharm-debugger
-ROS_ENV_SETUP="/opt/ros/${ROS_DISTRO}/install/setup.bash"
-echo "sourcing ${ROS_ENV_SETUP}"
-source "${ROS_ENV_SETUP}"
