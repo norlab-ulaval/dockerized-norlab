1875e0c9537aef05abc39b066641aa41c0987ebf
RedLeaderOne <luc.coupal.1@ulaval.ca>
2023-09-29 16:24:08 -0400

    NMO-337 | Cleaned project-cicd Dockerfile; addedd requirement.bash; mod alias;

---
+++ dockerized-norlab-images/core-images/dn-project/project-core/requirement.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# ToDo: add `COPY requirement.bash . \ RUN source requirement.bash` steps to dockerfile
+# Inspiration https://github.com/winstxnhdw/AutoCarROS2/blob/master/requirements.sh
