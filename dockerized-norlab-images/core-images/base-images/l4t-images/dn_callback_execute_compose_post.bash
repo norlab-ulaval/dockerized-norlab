f4ba6f6c040c5891729750f820ca4796a0882770
RedLeaderOne <luc.coupal.1@ulaval.ca>
2024-02-11 21:14:37 -0500

    feat: l4t base image mimic to amd64 logic for multi-arch build [NMO-381]
    - fix multiarch manifeste
    
    [skip release]

--- dockerized-norlab-images/core-images/base-images/l4t-images/dn_callback_execute_compose_post.bash
+++ dockerized-norlab-images/core-images/base-images/l4t-images/dn_callback_execute_compose_post.bash
@@ -4,12 +4,10 @@ function dn::callback_execute_compose_post() {
 
     n2st::print_msg "Preparing docker manifeste for multiarch image"
 
-    docker manifest create --amend "${DN_HUB:?err}/dockerized-norlab-base-image:${DN_IMAGE_TAG:?err}" \
+    docker buildx imagetools create --tag "${DN_HUB:?err}/dockerized-norlab-base-image:${DN_IMAGE_TAG:?err}" \
          "${DN_HUB}/dockerized-norlab-base-image:${DN_IMAGE_TAG}-arm64" \
          "${DN_HUB}/dockerized-norlab-base-image:${DN_IMAGE_TAG}-amd64"
 
-    docker manifest push "${DN_HUB}/dockerized-norlab-base-image:${DN_IMAGE_TAG}"
-
-    n2st::print_msg_done "Multiarch image push to docker registry"
+    n2st::print_msg_done "Multiarch image pushed to docker registry"
 
 }
