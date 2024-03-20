2232950abe738c5261b9b9ea22cfe1c736ebf60a
RedLeaderOne <luc.coupal.1@ulaval.ca>
2024-03-20 03:14:52 -0400

    fix(project-develop/deploy): ros2 multi-architecture compilation steps

--- dockerized-norlab-images/core-images/dn-project/project-develop/dn_ros2_rebuild_dev_workspace.bash
+++ dockerized-norlab-images/core-images/dn-project/project-develop/dn_ros2_rebuild_dev_workspace.bash
@@ -3,6 +3,9 @@
 #set -e # exit script if any statement returns a non-true return value
 #set -v
 
+PARAM_ARCH=$1
+
+
 cd "${DN_DEV_WORKSPACE}"
 
 # Install dependencies
@@ -12,8 +15,23 @@ rosdep install --from-paths ./src --ignore-packages-from-source --rosdistro "${R
 
 ## colcon build step: rebuild everything in the catkin workspace DN_DEV_WORKSPACE
 source "/opt/ros/${ROS_DISTRO}/setup.bash"
-colcon build --symlink-install
-# --merge-install
+
+COLCON_FLAGS=()
+if [[ ${PARAM_ARCH} == 'native' ]]; then
+  echo -e "Builder is running on native architecture"
+  COLCON_FLAGS+=(--symlink-install)
+elif [[ ${PARAM_ARCH} == 'virtualization' ]]; then
+  echo -e "Builder is running in architecture virtualisation"
+  COLCON_FLAGS+=(--executor sequential)
+fi
+COLCON_FLAGS+=(
+  --cmake-clean-cache
+  --cmake-args -DCMAKE_BUILD_TYPE=Release
+  --event-handlers console_direct+
+)
+
+echo -e "COLCON_FLAGS=( ${COLCON_FLAGS[*]} )"
+colcon build ${COLCON_FLAGS[@]}
 source "${DN_DEV_WORKSPACE}/install/setup.bash"
 
 cd "${DN_PROJECT_PATH}"
