7b3364d6f453da99eb14ec9870ce87d9aea2ff5c
RedLeaderOne <luc.coupal.1@ulaval.ca>
2024-07-23 18:36:23 -0400

    fix: disable shellcheck warning SC2068 in build script [NMO-551]
    - Might be the cause of failure
    - The shellcheck warning SC2068 has been suppressed in the dn_build_all.bash script.
    - This change is specifically within the command that executes the 'dn_execute_compose_over_build_matrix.bash' script.

--- dockerized-norlab-scripts/build_script/dn_build_all.bash
+++ dockerized-norlab-scripts/build_script/dn_build_all.bash
@@ -87,9 +87,10 @@ ${MSG_END_FORMAT}"
 
 
   cd "${DN_PATH:?'Variable not set'}" || exit 1
+  # shellcheck disable=SC2068
   bash ./dockerized-norlab-scripts/build_script/dn_execute_compose_over_build_matrix.bash \
                         "${NBS_BUILD_MATRIX_CONFIG:?'Variable not set'}/$EACH_BUILD_MATRIX" \
-                        "$@" -- "${DOCKER_COMMAND_W_FLAGS}"
+                        $@ -- "${DOCKER_COMMAND_W_FLAGS}"
 
     dn::fetch_build_log
     dn::agregate_build_logs
