0ee9007926542b118b75868947bf4f1423e2b0a7
RedLeaderOne <luc.coupal.1@ulaval.ca>
2024-07-26 13:26:34 -0400

    feat: enhance entrypoint [NMO-548]
    - Add a check to the SSH entrypoint to prevent redundant sshd daemon launches.
    - Reorder build arguments in Dockerfile for clearer declarations.

--- dockerized-norlab-images/core-images/dn-project/project-develop/dn_ros2_entrypoint.bash
+++ dockerized-norlab-images/core-images/dn-project/project-develop/dn_ros2_entrypoint.bash
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -e  # exit script if any statement returns a non-true return value
 
-echo
-echo -e "Starting container \033[1;37m internal ssh server for IDE remote development workflow on port ${DN_SSH_SERVER_PORT}\033[0m with \033[1;37m user ${DN_SSH_SERVER_USER}\033[0m (default pass: lasagne)"
+# Check if sshd is running
+if ! pgrep -x "sshd" > /dev/null
+then
+  echo
+  echo -e "Starting container \033[1;37m internal ssh server for IDE remote development workflow on port ${DN_SSH_SERVER_PORT}\033[0m with \033[1;37m user ${DN_SSH_SERVER_USER}\033[0m (default pass: lasagne)"
 
-LAUNCH_SSN_DAEMON=( '/usr/sbin/sshd' '-D' '-e' '-f' '/etc/ssh/sshd_config_dockerized_norlab_openssh_server' )
-#LAUNCH_SSN_DAEMON=( '/usr/sbin/sshd' '-e' '-f' '/etc/ssh/sshd_config_dockerized_norlab_openssh_server' )
-# Note on sshd flags:
-# -D : sshd will not detach and does not become a daemon. This allows easy monitoring of sshd.
-# -e : sshd will send the output to the standard error instead of the system log.
-# -f : config_file
-if [[ $(whoami) == "root" ]]; then
-  "${LAUNCH_SSN_DAEMON[@]}"
+  #LAUNCH_SSN_DAEMON=( '/usr/sbin/sshd' '-D' '-e' '-f' '/etc/ssh/sshd_config_dockerized_norlab_openssh_server' )
+  LAUNCH_SSN_DAEMON=( '/usr/sbin/sshd' '-e' '-f' '/etc/ssh/sshd_config_dockerized_norlab_openssh_server' )
+  # Note on sshd flags:
+  # -D : sshd will not detach and does not become a daemon. This allows easy monitoring of sshd.
+  # -e : sshd will send the output to the standard error instead of the system log.
+  # -f : config_file
+  if [[ $(whoami) == "root" ]]; then
+    "${LAUNCH_SSN_DAEMON[@]}"
+  else
+    echo "Launch the ssh daemon in a subshell as root"
+    sudo bash -c "${LAUNCH_SSN_DAEMON[*]}"
+  fi
 else
-  echo "Launch the ssh daemon in a subshell as root"
-  sudo bash -c "${LAUNCH_SSN_DAEMON[*]}"
+  echo -e "ssh daemon is already running."
 fi
 
 
