c4b47d38046407d7893788ed30d44b705ec0d25b
RedLeaderOne <luc.coupal.1@ulaval.ca>
2024-07-31 17:33:19 -0400

    fix(entrypoint): correct error handling and add debug messages
    - Corrected error message condition in dn_entrypoint scripts
    - Added debug messages based on DN_ENTRYPOINT_SHOW_DEBUG flag
    - Set DN_ENTRYPOINT_SHOW_DEBUG to true in Dockerfile.template for better
      traceability

--- dockerized-norlab-images/core-images/dn-project/project-deploy/dn_entrypoint.attach.bash
+++ dockerized-norlab-images/core-images/dn-project/project-deploy/dn_entrypoint.attach.bash
@@ -5,15 +5,19 @@ set -e  # exit script if any statement returns a non-true return value
 
 # ====DN-project internal logic====================================================================
 
-
-# ====DN-project user defined logic================================================================
-
 # ....Load library.................................................................................
 source /import_dockerized_norlab_container_tools.bash
 n2st::set_which_python3_version && test -n "${PYTHON3_VERSION}" || exit 1
-if [[ -n "${PYTHON3_VERSION}" ]]; then
-  echo -e "[\033[1;31mERROR\033[0m] dn_entrypoint.init.bash | Script import_dockerized_norlab_container_tools.bash failled" 1>&2
+if [[ -z "${PYTHON3_VERSION}" ]]; then
+  echo -e "[\033[1;31mERROR\033[0m] $0 | Script import_dockerized_norlab_container_tools.bash failled" 1>&2
 fi
+
+if [[ ${DN_ENTRYPOINT_TRACE_EXECUTION} == true ]]; then
+  n2st::print_msg "Execute $0"
+fi
+
+
+# ====DN-project user defined logic================================================================
 
 # ....Execute DN-project user callback.............................................................
 # Sanity check
--- dockerized-norlab-images/core-images/dn-project/project-develop/dn_entrypoint.init.bash
+++ dockerized-norlab-images/core-images/dn-project/project-develop/dn_entrypoint.init.bash
@@ -4,6 +4,16 @@ set -e  # exit script if any statement returns a non-true return value
 # (CRITICAL) ToDo: NMO-557 test: add missing dn_entrypoint.*.bash unit-tests
 
 # ====DN-project internal logic====================================================================
+# ....Load library.................................................................................
+source /import_dockerized_norlab_container_tools.bash
+n2st::set_which_python3_version && test -n "${PYTHON3_VERSION}" || exit 1
+if [[ -z "${PYTHON3_VERSION}" ]]; then
+  echo -e "[\033[1;31mERROR\033[0m] $0 | Script import_dockerized_norlab_container_tools.bash failled" 1>&2
+fi
+
+if [[ ${DN_ENTRYPOINT_TRACE_EXECUTION} == true ]]; then
+  n2st::print_msg "Execute $0"
+fi
 
 # ....SSH daemon...................................................................................
 # Check if sshd is running
@@ -24,12 +34,6 @@ fi
 
 # ====DN-project user defined logic================================================================
 
-# ....Load library.................................................................................
-source /import_dockerized_norlab_container_tools.bash
-n2st::set_which_python3_version && test -n "${PYTHON3_VERSION}" || exit 1
-if [[ -n "${PYTHON3_VERSION}" ]]; then
-  echo -e "[\033[1;31mERROR\033[0m] dn_entrypoint.init.bash | Script import_dockerized_norlab_container_tools.bash failled" 1>&2
-fi
 
 # ....Execute DN-project user callback.............................................................
 # Sanity check
