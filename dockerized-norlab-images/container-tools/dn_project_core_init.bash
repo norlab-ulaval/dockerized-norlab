da230f0a040c16038f072a76dba88a3dcd555fcb
RedLeaderOne <luc.coupal.1@ulaval.ca>
2024-08-02 14:05:10 -0400

    fix(dn_project_core_init.bash): go back to initial dir on exit
    - Added validation checks for DN_PROJECT_USER_HOME and DN_PROJECT_PATH
      directories in Dockerfile.template.
    - Refactored dn_project_core_init.bash to encapsulate logic in a
      function and ensure script sourcing.
    - Included error message if script is not sourced correctly and
      improved readability of initialization script.
    
    Issue NMO-548, RLRP-213

--- dockerized-norlab-images/container-tools/dn_project_core_init.bash
+++ dockerized-norlab-images/container-tools/dn_project_core_init.bash
@@ -20,66 +20,84 @@ set -e
 
 # (CRITICAL) ToDo: unit-test (ref task NMO-548 and RLRP-213)
 
-# ....Check pre-conditions.........................................................................
-{
-  test -n "${DN_PROJECT_USER:?'Env variable need to be set and non-empty.'}"
-  test -n "${DN_PROJECT_USER_HOME:?'Env variable need to be set and non-empty.'}"
-  test -n "${DN_PROJECT_UID:?'Env variable need to be set and non-empty.'}"
-  test -n "${DN_PROJECT_GID:?'Env variable need to be set and non-empty.'}"
-  test -n "${DN_PROJECT_PATH:?'Env variable need to be set and non-empty.'}"
-  test -n "${DN_DEV_WORKSPACE:?'Env variable need to be set and non-empty.'}"
-  test -n "${DN_PROJECT_GIT_NAME:?'Env variable need to be set and non-empty.'}"
+function dn::initialize_dockerized_norlab_project() {
+
+  # ....Check pre-conditions.........................................................................
+  {
+    test -n "${DN_PROJECT_USER:?'Env variable need to be set and non-empty.'}"
+    test -n "${DN_PROJECT_USER_HOME:?'Env variable need to be set and non-empty.'}"
+    test -n "${DN_PROJECT_UID:?'Env variable need to be set and non-empty.'}"
+    test -n "${DN_PROJECT_GID:?'Env variable need to be set and non-empty.'}"
+    test -n "${DN_PROJECT_PATH:?'Env variable need to be set and non-empty.'}"
+    test -n "${DN_DEV_WORKSPACE:?'Env variable need to be set and non-empty.'}"
+    test -n "${DN_PROJECT_GIT_NAME:?'Env variable need to be set and non-empty.'}"
+  }
+  # ....Create new user and home.....................................................................
+
+  # Inspired from https://roboticseabass.com/2023/07/09/updated-guide-docker-and-ros2/
+  {
+    groupadd --force --gid "${DN_PROJECT_GID}" "${DN_PROJECT_USER}"
+    useradd --uid "${DN_PROJECT_UID}" --gid "${DN_PROJECT_GID}" --create-home "${DN_PROJECT_USER}"
+    echo "${DN_PROJECT_USER} ALL=(root) NOPASSWD:ALL" >/etc/sudoers.d/"${DN_PROJECT_USER}"
+    chmod 0440 "/etc/sudoers.d/${DN_PROJECT_USER}"
+    mkdir -p "${DN_PROJECT_USER_HOME}"
+    chown -R "${DN_PROJECT_UID}":"${DN_PROJECT_GID}" "${DN_PROJECT_USER_HOME}"
+
+    # Add the 'video' groups to new user as it's required for GPU access.
+    # (not a problem on norlab-og but mandatory on Jetson device)
+    # Ref: https://forums.developer.nvidia.com/t/how-to-properly-create-new-users/68660/2
+    usermod -a -G video,sudo "${DN_PROJECT_USER}"
+  }
+
+  # ....Setup project dev workspace..................................................................
+  {
+    mkdir -p "${DN_PROJECT_PATH}"
+    chown -R "${DN_PROJECT_UID}":"${DN_PROJECT_GID}" "${DN_PROJECT_PATH}"
+
+    # (Priority) ToDo: Not sure its usefull to symlink those (ref task NMO-548)
+    # Add useful simlink in user root dir
+    ln -s "${DN_DEV_WORKSPACE}" "${DN_PROJECT_USER_HOME}${DN_DEV_WORKSPACE}"
+    ln -s "${DN_PROJECT_PATH}" "${DN_PROJECT_USER_HOME}${DN_PROJECT_GIT_NAME}"
+  }
+
+  # ....Simlink and change ownership of DN container-tools...........................................
+  {
+    DN_CONTAINER_TOOLS_DIR=/dockerized-norlab/dockerized-norlab-images/container-tools
+    ln -s "${DN_CONTAINER_TOOLS_DIR}/dn_info.bash" "${DN_PROJECT_USER_HOME}/dn_info.bash"
+    ln -s "${DN_CONTAINER_TOOLS_DIR}/import_dockerized_norlab_container_tools.bash" "${DN_PROJECT_USER_HOME}/import_dockerized_norlab_container_tools.bash"
+  }
+
+  {
+    #cd /dockerized-norlab/dockerized-norlab-images/container-tools
+    cd "${DN_PROJECT_USER_HOME}"
+    source import_dockerized_norlab_container_tools.bash
+    test -n "${DN_PATH:?'Env variable need to be set by import_dockerized_norlab_container_tools.bash and non-empty.'}"
+    chown -R "${DN_PROJECT_UID}":"${DN_PROJECT_GID}" "${DN_PATH}"
+    git config --system --add safe.directory "*"
+  }
+
+  # ....Add all dockerized-norlab accumulated bashrc instruction to project user bashrc..............
+  (
+    echo ""
+    echo "# >>> dockerized-norlab bashrc"
+    echo "source /dockerized-norlab/dockerized-norlab-images/container-tools/dn_bashrc.bash"
+    echo ""
+  ) >>"${DN_PROJECT_USER_HOME}/.bashrc"
+
+  return 0
 }
-# ....Create new user and home.....................................................................
 
-# Inspired from https://roboticseabass.com/2023/07/09/updated-guide-docker-and-ros2/
-{
-  groupadd --force --gid "${DN_PROJECT_GID}" "${DN_PROJECT_USER}"
-  useradd --uid "${DN_PROJECT_UID}" --gid "${DN_PROJECT_GID}" --create-home "${DN_PROJECT_USER}"
-  echo "${DN_PROJECT_USER} ALL=(root) NOPASSWD:ALL" >/etc/sudoers.d/"${DN_PROJECT_USER}"
-  chmod 0440 "/etc/sudoers.d/${DN_PROJECT_USER}"
-  mkdir -p "${DN_PROJECT_USER_HOME}"
-  chown -R "${DN_PROJECT_UID}":"${DN_PROJECT_GID}" "${DN_PROJECT_USER_HOME}"
-
-  # Add the 'video' groups to new user as it's required for GPU access.
-  # (not a problem on norlab-og but mandatory on Jetson device)
-  # Ref: https://forums.developer.nvidia.com/t/how-to-properly-create-new-users/68660/2
-  usermod -a -G video,sudo "${DN_PROJECT_USER}"
-}
-
-# ....Setup project dev workspace..................................................................
-{
-  mkdir -p "${DN_PROJECT_PATH}"
-  chown -R "${DN_PROJECT_UID}":"${DN_PROJECT_GID}" "${DN_PROJECT_PATH}"
-
-  # (Priority) ToDo: Not sure its usefull to symlink those (ref task NMO-548)
-  # Add useful simlink in user root dir
-  ln -s "${DN_DEV_WORKSPACE}" "${DN_PROJECT_USER_HOME}${DN_DEV_WORKSPACE}"
-  ln -s "${DN_PROJECT_PATH}" "${DN_PROJECT_USER_HOME}${DN_PROJECT_GIT_NAME}"
-}
-
-# ....Simlink and change ownership of DN container-tools...........................................
-{
-  DN_CONTAINER_TOOLS_DIR=/dockerized-norlab/dockerized-norlab-images/container-tools
-  ln -s "${DN_CONTAINER_TOOLS_DIR}/dn_info.bash" "${DN_PROJECT_USER_HOME}/dn_info.bash"
-  ln -s "${DN_CONTAINER_TOOLS_DIR}/import_dockerized_norlab_container_tools.bash" "${DN_PROJECT_USER_HOME}/import_dockerized_norlab_container_tools.bash"
-}
-
-{
-  #cd /dockerized-norlab/dockerized-norlab-images/container-tools
-  cd "${DN_PROJECT_USER_HOME}"
-  source import_dockerized_norlab_container_tools.bash
-  test -n "${DN_PATH:?'Env variable need to be set by import_dockerized_norlab_container_tools.bash and non-empty.'}"
-  chown -R "${DN_PROJECT_UID}":"${DN_PROJECT_GID}" "${DN_PATH}"
-  git config --system --add safe.directory "*"
-}
-
-# ....Add all dockerized-norlab accumulated bashrc instruction to project user bashrc..............
-(
-  echo ""
-  echo "# >>> dockerized-norlab bashrc"
-  echo "source /dockerized-norlab/dockerized-norlab-images/container-tools/dn_bashrc.bash"
-  echo ""
-) >>"${DN_PROJECT_USER_HOME}/.bashrc"
-
-return 0
+# ::::Main:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+  # This script is being run, ie: __name__="__main__"
+  MSG_ERROR_FORMAT="\033[1;31m"
+  MSG_END_FORMAT="\033[0m"
+  echo -e "${MSG_ERROR_FORMAT}[ERROR]${MSG_END_FORMAT} This script must be sourced!
+        i.e.: $ source $(basename "$0")" 1>&2
+  exit 1
+else
+  # This script is being sourced, ie: __name__="__source__"
+  pushd "$(pwd)"
+  dn::initialize_dockerized_norlab_project
+  popd
+fi
