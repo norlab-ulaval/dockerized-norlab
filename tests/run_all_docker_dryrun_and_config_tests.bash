fcac66fe9511ce7d3d6fa8c67514f25fb26a8b26
RedLeader962 <redleader962@gmail.com>
2025-06-01 19:28:58 -0400

    refactor(project-deploy): cleanup scripts and streamline README links
    - Updated README to include accurate links and descriptions for managing mocking scripts in related repos.
    - Removed redundant commented-out commands and adjusted Dockerfile debug statements for better clarity.
    - Simplified and organized test bash scripts, ensuring proper error handling and consistent script structure.
    
    Issue NMO-661

--- tests/run_all_docker_dryrun_and_config_tests.bash
+++ tests/run_all_docker_dryrun_and_config_tests.bash
@@ -7,14 +7,13 @@
 #
 #
 
-_PATH_TO_SCRIPT="$(realpath "$0")"
-SCRIPT_DIR_PATH="$(dirname "${_PATH_TO_SCRIPT}")"
-TEST_DIR="$SCRIPT_DIR_PATH/tests_docker_dryrun_and_config"
-
 set -e            # exit on error
 set -o nounset    # exit on unbound variable
 set -o pipefail   # exit if errors within pipes
 
+_PATH_TO_SCRIPT="$(realpath "$0")"
+SCRIPT_DIR_PATH="$(dirname "${_PATH_TO_SCRIPT}")"
+TEST_DIR="$SCRIPT_DIR_PATH/tests_docker_dryrun_and_config"
 
 source "${SCRIPT_DIR_PATH}/../utilities/norlab-build-system/src/utility_scripts/nbs_run_all_test_and_dryrun_in_directory.bash" "${TEST_DIR}"
-
+exit $?
