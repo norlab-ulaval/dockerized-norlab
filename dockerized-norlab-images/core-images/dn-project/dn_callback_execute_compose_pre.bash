91ea3b4ed1f32fa31e765b5eafddf2b8f9a4a069
RedLeader962 <redleader962@gmail.com>
2025-06-23 10:24:49 -0400

    fix(scripts): improve error handling for pre/post compose callbacks
    - Replaced `exit` commands with `return` to support embedded script execution.
    - Enhanced error messaging structure for consistency and clarity.
    - Updated sanity checks for greater robustness and maintainability.
    
    Issue NMO-714

--- dockerized-norlab-images/core-images/dn-project/dn_callback_execute_compose_pre.bash
+++ dockerized-norlab-images/core-images/dn-project/dn_callback_execute_compose_pre.bash
@@ -14,14 +14,12 @@
 #
 # =================================================================================================
 function dn::callback_execute_compose_pre() {
-  set -e
-
   n2st::print_formated_script_header "dn_callback_execute_compose_pre.bash" "${MSG_LINE_CHAR_UTIL}"
 
-  test -n "${DN_PATH:?err}" || n2st::print_msg_error_and_exit "Env variable DN_PATH need to be set and non-empty."
-  test -d "${DN_PATH}/utilities/tmp" || n2st::print_msg_error_and_exit "The directory ${DN_PATH}/utilities/tmp is unreachable"
+  test -n "${DN_PATH:?err}" || { n2st::print_msg_error "Env variable DN_PATH need to be set and non-empty." && return 1 ;}
+  test -d "${DN_PATH}/utilities/tmp" || { n2st::print_msg_error "The directory ${DN_PATH}/utilities/tmp is unreachable" && return 1 ;}
 
-  if [[ -d "${DN_PATH}/utilities/tmp/dockerized-norlab-project-mock" ]]; then
+  if [[ -d "${DN_PATH}/utilities/tmp/dockerized-norlab-project-mock/.git" ]]; then
       # Delete git cloned repo
       rm -rf "${DN_PATH}/utilities/tmp/dockerized-norlab-project-mock"
   fi
@@ -30,14 +28,14 @@ function dn::callback_execute_compose_pre() {
   fi
   git clone https://github.com/norlab-ulaval/dockerized-norlab-project-mock.git \
     "${DN_PATH}/utilities/tmp/dockerized-norlab-project-mock" \
-    || n2st::print_msg_error_and_exit "Could not clone dockerized-norlab-project-mock"
+    || { n2st::print_msg_error "Could not clone dockerized-norlab-project-mock" && return 1 ;}
 
-  # ....Saniti check...............................................................................
-  test -d "${DN_PATH}/utilities/tmp" || n2st::print_msg_error_and_exit "The directory ${DN_PATH}/utilities/tmp is unreachable"
+  # ....Sanity check...............................................................................
+  test -d "${DN_PATH}/utilities/tmp" || { n2st::print_msg_error "The directory ${DN_PATH}/utilities/tmp is unreachable" && return 1 ;}
   test -d "${DN_PATH}/utilities/tmp/dockerized-norlab-project-mock/.git" \
   || { \
     tree -a -L 2 "${DN_PATH}/utilities/tmp" &&
-    n2st::print_msg_error_and_exit "The directory ${DN_PATH}/utilities/tmp is unreachable" ;
+    n2st::print_msg_error "The directory ${DN_PATH}/utilities/tmp is unreachable" && return 1 ;
     }
 
   n2st::print_formated_script_footer "dn_callback_execute_compose_pre.bash" "${MSG_LINE_CHAR_UTIL}"
