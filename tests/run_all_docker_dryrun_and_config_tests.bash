68a70cb3ad47a86a33c24848d781b49de43174b1
RedLeader962 <redleader962@gmail.com>
2025-06-23 09:22:21 -0400

    refactor(docker): improve dry-run test script and enhance Docker setup
    - Simplified test script by removing redundant error-handling commands.
    - Refined Dockerfiles for clearer and more maintainable build processes.
    - Adjusted CMake configuration to use consistent install path definition.
    
    Issue NMO-714, NMO-602, NMO-605

--- tests/run_all_docker_dryrun_and_config_tests.bash
+++ tests/run_all_docker_dryrun_and_config_tests.bash
@@ -9,17 +9,10 @@
 
 # ....Setup........................................................................................
 source "$(git rev-parse --show-toplevel)/import_dockerized_norlab_tools.bash" || exit 1
+test_dir="${DN_PATH:?err}/tests/tests_docker_dryrun_and_config"
 
 # ....Begin........................................................................................
-(
-  set -e            # exit on error
-  set -o nounset    # exit on unbound variable
-  set -o pipefail   # exit if errors within pipes
-
-  test_dir="${DN_PATH:?err}/tests/tests_docker_dryrun_and_config"
-  source "${NBS_PATH:?err}/src/utility_scripts/nbs_run_all_test_and_dryrun_in_directory.bash" "${test_dir}"
-)
-#exit $?
+source "${NBS_PATH:?err}/src/utility_scripts/nbs_run_all_test_and_dryrun_in_directory.bash" "${test_dir}"
 
 
 
