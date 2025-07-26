81a9f0b2c6b907b6a948482d67348430ee0d5aa0
RedLeader962 <redleader962@gmail.com>
2025-07-26 18:08:28 -0400

    fix: solve the ros2 apt repository problem and improve Dockerfile error handling
    - Fix silent error on apt repository configuration
    - Added error handling using `exit 1` to improve Dockerfile resilience.
    - Consolidated and cleaned redundant commands like `apt-get autoremove`.
    - Improved readability of scripts by restructuring and aligning sections.
    - Enhanced user-feedback messaging to stderr for better debugging experience.
    
    Issue NMO-756

--- dockerized-norlab-images/container-tools/dn_project_core_init.bash
+++ dockerized-norlab-images/container-tools/dn_project_core_init.bash
@@ -32,9 +32,9 @@ function dn::initialize_dockerized_norlab_project() {
     test -n "${DN_PROJECT_PATH:?'Env variable need to be set and non-empty.'}" && \
     test -n "${DN_DEV_WORKSPACE:?'Env variable need to be set and non-empty.'}" && \
     test -n "${DN_PROJECT_GIT_NAME:?'Env variable need to be set and non-empty.'}" ;
-  } || exit
-  # ....Create new user and home...................................................................
+  } || n2st::print_msg_error_and_return "Failed pre-condition check!"
 
+  # ....Create new user and home...................................................................
   # Inspired from https://roboticseabass.com/2023/07/09/updated-guide-docker-and-ros2/
   {
     groupadd --force --gid "${DN_PROJECT_GID}" "${DN_PROJECT_USER}"
@@ -48,7 +48,7 @@ function dn::initialize_dockerized_norlab_project() {
     # (not a problem on norlab-og but mandatory on Jetson device)
     # Ref: https://forums.developer.nvidia.com/t/how-to-properly-create-new-users/68660/2
     usermod -a -G video,sudo "${DN_PROJECT_USER}"
-  }
+  } || n2st::print_msg_error_and_return "Failed new user ${DN_PROJECT_USER} setup!"
 
   # ....Setup project dev workspace................................................................
   {
@@ -90,14 +90,14 @@ function dn::initialize_dockerized_norlab_project() {
 # ::::Main:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
   # This script is being run, ie: __name__="__main__"
-  MSG_ERROR_FORMAT="\033[1;31m"
-  MSG_END_FORMAT="\033[0m"
-  echo -e "${MSG_ERROR_FORMAT}[ERROR]${MSG_END_FORMAT} This script must be sourced!
+  dna_error_prefix="\033[1;31m[DNA error]\033[0m"
+  echo -e "${dna_error_prefix} This script must be sourced!
         i.e.: $ source $(basename "$0")" 1>&2
   exit 1
 else
   # This script is being sourced, ie: __name__="__source__"
-  dn::initialize_dockerized_norlab_project
+  test -n "$( declare -f n2st::print_msg )" || { echo -e "\033[1;31m[N2ST error]\033[0m The N2ST lib is not loaded!" 1>&2 && exit 1; }
+  dn::initialize_dockerized_norlab_project || n2st::print_msg_error_and_exit "dn::initialize_dockerized_norlab_project exited with error!"
 fi
 
 # ====Teardown=====================================================================================
--- dockerized-norlab-images/core-images/dn-project/project-core/project_requirements/shell.requirements.bash
+++ dockerized-norlab-images/core-images/dn-project/project-core/project_requirements/shell.requirements.bash
@@ -1,4 +1,43 @@
 #!/bin/bash
+# =================================================================================================
+# DNA shell requirements install
+#
+# Notes:
+# - This file is used in .dockerized_norlab/configuration/Dockerfile via pip install
+# - It is executed before python.requirements.txt
+# - N2ST library is available in script i.e., shell script function prefixed 'n2st::'
+#
+# =================================================================================================
 
-# ToDo: add `COPY shell.requirements.bash . \ RUN source shell.requirements.bash` steps to dockerfile
-# Inspiration https://github.com/winstxnhdw/AutoCarROS2/blob/master/requirements.sh
+# ....Example 1....................................................................................
+{
+  apt-get update \
+  && apt-get install --assume-yes --no-install-recommends \
+    vim \
+    tree \
+  && rm -rf /var/lib/apt/lists/* \
+  && apt-get clean ;
+} || n2st::print_msg_error_and_exit "Failed apt-get package install!"
+# .................................................................................................
+
+# ....Example 2....................................................................................
+if [[ -f "${DN_PROJECT_PATH:?err}/requirements.txt"  ]]; then
+  n2st::print_msg "Execute pip install from repository root python requirement file..."
+  pip3 install --verbose -r "${DN_PROJECT_PATH}/requirements.txt" \
+    || n2st::print_msg_error_and_exit "Failed pip install from repository root requirement!"
+fi
+# .................................................................................................
+
+# ....Example 3....................................................................................
+if [[ $( n2st::which_architecture_and_os ) == "l4t\arm64" ]]; then
+  n2st::print_msg "Is running on a Jetson..."
+  # Add Jetson logic e.g., cat /proc/device-tree/model
+fi
+# .................................................................................................
+
+# ....Example 4....................................................................................
+if [[ $( n2st::which_python3_version ) == 3.10 ]]; then
+  n2st::print_msg "Execute python 3.10 specialized install..."
+  # Add python logic
+fi
+# .................................................................................................
