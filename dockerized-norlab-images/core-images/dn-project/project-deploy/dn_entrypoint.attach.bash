3e2d7c46cbe1b4e7e776e4909c0a18d70a70ed1a
RedLeader962 <redleader962@gmail.com>
2025-08-05 15:04:21 -0400

    feat(scripts): improve trace execution and error message formatting
    - Replaced script-specific error and trace message prefixes with inlined ANSI
      codes for consistency and readability across all entrypoint and config files.
    - Enhanced trace execution by adding conditional checks and aligning with
      `DN_ENTRYPOINT_TRACE_EXECUTION` variable usage for better debugging control.
    - Updated trace logging to provide clearer context by explicitly naming
      executed scripts.
    
    Issue NMO-770

--- dockerized-norlab-images/core-images/dn-project/project-deploy/dn_entrypoint.attach.bash
+++ dockerized-norlab-images/core-images/dn-project/project-deploy/dn_entrypoint.attach.bash
@@ -10,24 +10,23 @@ set -e  # exit script if any statement returns a non-true return value
 ## (CRITICAL) ToDo: validate >> deleting DN lib import ↓ (ref task NMO-770)
 #source /import_dockerized_norlab_container_tools.bash
 
-error_prefix="\033[1;31m[DN error]\033[0m"
-trace_prefix="\033[1;33m[DN trace]\033[0m"
+if [[ ${DN_ENTRYPOINT_TRACE_EXECUTION} == true ]]; then
+  echo -e "\033[1;33m[DN trace]\033[0m Execute dn_entrypoint.attach.bash"
+fi
+
 if [[ $- == *i* ]]; then
     if [[ "${DN_ENTRYPOINT_TRACE_EXECUTION}" == true ]]; then
-      echo -e "${trace_prefix} Interactive shell. Sourcing DN lib is handled via .bashrc"
+      echo -e "\033[1;33m[DN trace]\033[0m Interactive shell. Sourcing DN lib is handled via .bashrc"
     fi
 else
     if [[ "${DN_ENTRYPOINT_TRACE_EXECUTION}" == true ]]; then
-      echo -e "${trace_prefix} Non-interactive shell. Sourcing DN lib"
+      echo -e "\033[1;33m[DN trace]\033[0m Non-interactive shell. Sourcing DN lib"
     fi
     source /dockerized-norlab/dockerized-norlab-images/container-tools/bash_run_config/.bashrc.dn_non_interactive
 fi
 
-test -n "$( declare -f n2st::print_msg )" || { echo -e "${error_prefix} The N2ST lib is not loaded!" 1>&2 && exit 1; }
+test -n "$( declare -f n2st::print_msg )" || { echo -e "\033[1;31m[DN error]\033[0m The N2ST lib is not loaded!" 1>&2 && exit 1; }
 
-if [[ ${DN_ENTRYPOINT_TRACE_EXECUTION} == true ]]; then
-  echo -e "${trace_prefix} Execute $0"
-fi
 
 
 # ====DN-project user defined logic================================================================
